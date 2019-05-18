7ce3405fff6301b6ff9815103a28dadee0d1410e
Olivier <olivier.huguenot@airbus.com>
2019-05-18 12:40:08 +0200

    1st attempt…

---
+++ pt2257.h
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "debug.h"
+#include "SoftI2CMaster.h"
+
+class PT2257 {
+private:
+	uint16_t		analog_min_value;
+	uint16_t		analog_max_value;
+	uint8_t			analog_pin;
+	SoftI2CMaster *	softI2C;
+	void 			update(int aAnalogValue);
+public:
+	PT2257(uint8_t aAnalogInputPin);
+	PT2257(uint8_t aAnalogInputPin, uint16_t aAnalogMin, uint16_t aAnalogMax);
+	PT2257(uint8_t aAnalogInputPin, uint16_t aAnalogMin, uint16_t aAnalogMax, int8_t aSDAPin, int8_t aSCLPin);
+	void disable();
+	void scan();
+};
